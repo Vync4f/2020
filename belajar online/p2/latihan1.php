cf150142bd41c709efe7463f98fff96c9d737281
Vync4f <kafrawi3101@gmail.com>
2020-08-31 13:43:59 +0800

    menambahkan database dan file .php

---
+++ belajar online/p2/latihan1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+
+    <tr>
+
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NIM</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+
+    </tr>
+
+    <tr>
+      <td>1</td>
+      <td><img src="img/profile.jpg" width="60"></td>
+      <td>F1B019004</td>
+      <td>Achmad Evin Kafrawi</td>
+      <td>kafrawi3101@gmail.com</td>
+      <td>Teknik Elektro</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+
+    </tr>
+
+  </table>
+
+
+</body>
+
+</html>
